57eb0139e399d198b84af06465580ff3bdfbf42a
SEGARS <vim_branet@mail.ru>
2023-02-28 10:44:44 +0500

    лке

---
+++ exp13_Rec_mas/Program.cs
@@ -0,0 +1,12 @@
+﻿string[,] table = new string[2,5];
+// table[0,0]  table[0,1]  table[0,2]  table[0,4]  
+// table[1,0]  table[1,1]  table[1,2]  table[1,4]  
+table[1, 2] = "Слово";
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int colums = 0; colums < 2; colums++)
+    {
+        Console.WriteLine($"{table[rows, colums]}");
+    }
+}
